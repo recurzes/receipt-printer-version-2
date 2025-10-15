655498f62e5b8e15b31faead55ed35ab7f9d17b3
recurzes <donlancelotknight123@gmail.com>
2025-10-16 07:54:48 +0800

    feat: added configuration file for credentials

---
+++ src/util/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GEMINI_API_TOKEN = os.getenv("GEMINI_API_KEY")
+TODOIST_SECRET = os.getenv("TODOIST_SECRET")
+TODOIST_VERIFICATION_TOKEN = os.getenv("TODOIST_VERIFICATION_TOKEN")
+TODOIST_TEST_TOKEN = os.getenv("TODOIST_TEST_TOKEN")
+TODOIST_API_TOKEN = os.getenv("TODOIST_API_TOKEN")
