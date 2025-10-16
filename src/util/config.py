1e6c611e45feeeeb255b93772c59e6c19632dd89
recurzes <donlancelotknight123@gmail.com>
2025-10-16 08:06:53 +0800

    feat: added task CRUD operations using Todoist API and refactored routes into dedicated router

--- src/util/config.py
+++ src/util/config.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
+import todoist_api_python
+from todoist_api_python.api import TodoistAPI
 
 load_dotenv()
 
@@ -9,3 +11,5 @@ TODOIST_SECRET = os.getenv("TODOIST_SECRET")
 TODOIST_VERIFICATION_TOKEN = os.getenv("TODOIST_VERIFICATION_TOKEN")
 TODOIST_TEST_TOKEN = os.getenv("TODOIST_TEST_TOKEN")
 TODOIST_API_TOKEN = os.getenv("TODOIST_API_TOKEN")
+
+todoist = TodoistAPI(TODOIST_API_TOKEN)
