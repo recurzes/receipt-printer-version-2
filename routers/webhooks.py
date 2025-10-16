e9d834f2fa6a56ebd88e095969008aebf639d332
recurzes <donlancelotknight123@gmail.com>
2025-10-16 08:31:16 +0800

    feat: implemented detailed event handling for Todoist webhook

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -9,6 +9,7 @@ router = APIRouter(
     responses={401: {"description": "Unauthorized Boshet ka"}}
 )
 
+
 @router.post("/todoist")
 async def todoist_webhook(
         request: Request,
@@ -31,3 +32,70 @@ async def todoist_webhook(
     print(f"Delivery ID: {x_todoist_delivery_id}")
     print(f"User ID: {user_id}")
     print(f"Event data: {event_data}")
+
+    # Handle different event types:
+    if event_name == "item:added":
+        await handle_task_added(event_data)
+    elif event_name == "item:updated":
+        await handle_task_updated(event_data)
+    elif event_name == "item:completed":
+        await handle_task_completed(event_data)
+    elif event_name == "item:uncompleted":
+        await handle_task_uncompleted(event_data)
+    elif event_name == "item:deleted":
+        await handle_task_deleted(event_data)
+    elif event_name == "note:added":
+        await handle_note_added(event_data)
+    elif event_name == "note:updated":
+        await handle_note_updated(event_data)
+    elif event_name == "note:deleted":
+        await handle_note_deleted(event_data)
+
+    return {"status": "Success", "event": event_name}
+
+
+# Functions
+async def handle_task_added(event_data: dict):
+    task_id = event_data.get("id")
+    content = event_data.get("content")
+    print(f"New task added: {content} (ID: {task_id})")
+
+
+async def handle_task_updated(event_data: dict):
+    task_id = event_data.get("id")
+    content = event_data.get("content")
+    print(f"Task updated: {content} (ID: {task_id})")
+
+
+async def handle_task_completed(event_data: dict):
+    task_id = event_data.get("id")
+    content = event_data.get("content")
+    print(f"Task completed: {content} (ID: {task_id})")
+
+
+async def handle_task_uncompleted(event_data: dict):
+    task_id = event_data.get("id")
+    content = event_data.get("content")
+    print(f"Task reopened: {content} (ID: {task_id})")
+
+
+async def handle_task_deleted(event_data: dict):
+    task_id = event_data.get("id")
+    print(f"Task deleted: ID {task_id}")
+
+
+async def handle_note_added(event_data: dict):
+    note_id = event_data.get("id")
+    content = event_data.get("content")
+    print(f"Commend added: {content} (ID: {note_id}")
+
+
+async def handle_note_updated(event_data: dict):
+    note_id = event_data.get("id")
+    content = event_data.get("content")
+    print(f"Comment updated: {content} (ID: {note_id})")
+
+
+async def handle_note_deleted(event_data: dict):
+    note_id = event_data.get("id")
+    print(f"Comment deleted: ID {note_id}")
